fad1d7ed17531f026c52cdbfe7857b3e19ed9b40
kmoin1309 <128276854+kmoin1309@users.noreply.github.com>
2023-11-04 12:10:50 +0530

    Init
    Initial

---
+++ tailwind.config.js
@@ -0,0 +1,23 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["*"],
+  theme: {
+    extend: {fontFamily:{
+      mullish:['Mullish', 'sans-serif'],
+    },
+    colors: {
+        deepBlue: "#02042a",
+        LightBlue: "#2b84ea",
+        lightBlue300: "#4b94ed",
+        LightBLue500:"#0672e7",
+        greenLight: "#61cea6",
+        grayText: "#818597",
+        lightGray: "#e2e2e2",
+        grayBlue: "#344a6c", 
+        deepBlueHead: "#162f56",
+        gray2: "#525a76",
+    },},
+  },
+  plugins: [],
+}
+
